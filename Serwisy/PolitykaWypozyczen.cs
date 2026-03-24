cf64b0a87c57319129f8117e9c77891d6c5ea62b
Krystian Kierzkowski <s33673@pjwstk.edu.pl>
2026-03-24 17:32:39 +0100

    Dodanie polityki limitów i kar

---
+++ Serwisy/PolitykaWypozyczen.cs
@@ -0,0 +1,25 @@
+using WypozyczalniaSp.Model;
+
+namespace WypozyczalniaSprzetu.Serwisy;
+
+public class PolitykaWypozyczen
+{
+    public int PobierzLimitDla(Uzytkownik uzytkownik)
+    {
+        return uzytkownik.Typ switch
+        {
+            TypUzytkownika.Sluchacz => 2,
+            TypUzytkownika.PracownikUczelni => 5,
+            _ => 0
+        };
+    }
+
+    public decimal ObliczKare(DateTime terminZwrotu, DateTime dataZwrotu)
+    {
+        if (dataZwrotu.Date <= terminZwrotu.Date)
+            return 0;
+
+        int dniSpoznienia = (dataZwrotu.Date - terminZwrotu.Date).Days;
+        return dniSpoznienia * 15m;
+    }
+}
